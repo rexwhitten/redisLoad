b190ea0c0a4ddeaa6650df5d756e708ae34e9268
Rex <Rex.whitten@gmail.com>
2013-09-17 18:26:17 -0400

    First commit

---
+++ redisLoad/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Common;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.SqlServer;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace redisLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Sync .... ");
+
+            var program = SqlProgramParser.Parse("queries.txt");
+
+
+
+
+            Console.WriteLine("");
+        }
+
+
+
+        static int GetDataFromSql(String ConnectionStringName, String Query)
+        {
+            int error = 0;
+
+            try
+            {
+                Int32 redis_port = 6379;
+
+                error = 1;
+            }
+            catch(Exception x)
+            {
+                error = -1;
+                Console.WriteLine(x.Message);
+            }
+
+            return error;
+        }
+
+    }
+}
---
+++ redisLoad/SqlProgramParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace redisLoad
+{
+    public class SqlProgramParser
+    {
+        public static SqlProgram Parse(String SqlProgramFileName)
+        {
+            SqlProgram prog = new SqlProgram();
+
+            if (File.Exists(SqlProgramFileName))
+            {
+                string[] lines = File.ReadAllLines(SqlProgramFileName);
+                List<String> metaLines = new List<string>();
+                int line_number = 0;
+
+                // Scan for MEtA Lines 
+                foreach (var line in lines)
+                {
+                    line_number++; // Increment Line Number
+                    if (line.StartsWith("//"))
+                    {
+                        metaLines.Add(line); continue;
+                    }
+
+                    if (line.StartsWith("#"))
+                    {
+                        metaLines.Add(line);
+                        prog.ConnectionString = line.Remove(0, 1);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                // Scan for Query Lines 
+                String program = File.ReadAllText(SqlProgramFileName);
+                foreach (var meta in metaLines)
+                {
+                    program = program.Replace(meta, "");
+                }
+
+                prog.Queries = program.Split(';').ToList();
+
+
+            }
+            else
+            {
+                Console.WriteLine("String Does Not Exist. {0}", SqlProgramFileName);
+            }
+            return prog;
+        }
+    }
+
+    public class SqlProgram
+    {
+        public String ConnectionString { get; set; }
+        public List<String> Queries { get; set; }
+
+        public SqlProgram()
+        {
+            this.Queries = new List<string>();
+        }
+    }
+}
