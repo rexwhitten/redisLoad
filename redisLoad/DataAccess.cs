9574963f4389f4914546f05f08c1b0b514d810b0
Rex Whitten <rwhitten@acrowire.com>
2013-09-18 14:51:57 -0400

    First working version

---
+++ redisLoad/DataAccess.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redisLoad
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class DataAccess : IDisposable
+    {
+        #region [ Fields ]
+        private SqlConnection Connection;
+        private SqlTransaction CurrentTransaction;
+        private SqlCommand Command;
+        public String DatabaseName { get; set; }
+        #endregion
+
+        #region [ Constructors ]
+        public DataAccess()
+        {
+
+        }
+
+        public DataAccess(string ConnectionString)
+        {
+            this.CreateConnection(ConnectionString);
+            this.DatabaseName = this.Connection.Database;
+        }
+        #endregion
+
+        #region [ Connection Management Methods ]
+        /// <summary>
+        /// Initializes the Sql Connection Object 
+        /// </summary>
+        /// <param name="connectionStringName">ADO.NET Connection String</param>
+        public virtual void CreateConnection(string ConnectionString)
+        {
+            if ((this.CurrentTransaction == null))
+            {
+                Connection = new SqlConnection(ConnectionString);
+            }
+
+        }
+        #endregion
+
+        #region [ SQL Command Methods ]
+        public virtual void AddParameter(string name, object value, ParameterDirection direction)
+        {
+            if ((this.Command == null))
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+            SqlParameter parameter = new SqlParameter(name, this.GetValue(value));
+            parameter.Direction = direction;
+            this.Command.Parameters.Add(parameter);
+        }
+
+        public virtual void AddParameter(string name, object value, ParameterDirection direction, String UserDefinedTableTypeName)
+        {
+            if ((this.Command == null))
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+            SqlParameter parameter = new SqlParameter(name, SqlDbType.Structured, 0);
+            parameter.TypeName = UserDefinedTableTypeName;
+            parameter.SqlDbType = SqlDbType.Structured;
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            parameter.Direction = direction;
+            this.Command.Parameters.Add(parameter);
+        }
+
+        public virtual void AddParameter(System.Data.Common.DbParameter Parameter)
+        {
+            if ((this.Command == null))
+            {
+                return;
+            }
+            this.Command.Parameters.Add((SqlParameter)Parameter);
+        }
+
+        public virtual void CreateProcedureCommand(string storedProcedureName)
+        {
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+            this.Command = new SqlCommand(storedProcedureName, this.Connection);
+            this.Command.CommandType = CommandType.StoredProcedure;
+        }
+        #endregion
+
+        #region [ Projection Methods ]
+
+        public virtual DataSet ExecuteDataSet()
+        {
+            if ((this.Command == null))
+            {
+                throw new ApplicationException("The command object is not defined.");
+            }
+            if ((Connection == null))
+            {
+                if (this.DatabaseName == null)
+                {
+                    throw new ApplicationException("Call CreateConnection method before using the connection. Database Name is also blank.");
+                }
+            }
+
+
+            SqlDataAdapter da = null;
+            DataSet ds;
+            try
+            {
+                da = new SqlDataAdapter(Command);
+                ds = new DataSet();
+                da.Fill(ds);
+            }
+            catch (SqlException sqlException)
+            {
+                throw sqlException;
+            }
+            finally
+            {
+                if ((da != null))
+                {
+                    da.Dispose();
+                }
+            }
+            return ds;
+        }
+
+        public virtual SqlDataReader ExecuteDataReader()
+        {
+            if ((this.Command == null))
+            {
+                throw new ApplicationException("The command object is not defined.");
+            }
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+            ConnectionState initialState = ConnectionState.Closed;
+            SqlDataReader reader = null;
+            try
+            {
+                this.QuietOpen(out initialState);
+                reader = this.Command.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            return reader;
+        }
+
+        public virtual int ExecuteNonQuery()
+        {
+            if ((this.Command == null))
+            {
+                throw new ApplicationException("The command object is not defined.");
+            }
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+
+            ConnectionState initialState = ConnectionState.Closed;
+            int value;
+
+            try
+            {
+                this.QuietOpen(out initialState);
+
+                value = this.Command.ExecuteNonQuery();
+            }
+            catch (System.Exception x)
+            {
+                throw x;
+            }
+            finally
+            {
+                this.QuietClose(initialState);
+            }
+            return value;
+        }
+
+        public virtual object ExecuteScalar()
+        {
+            if ((this.Command == null))
+            {
+                throw new ApplicationException("The command object is not defined.");
+            }
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+            ConnectionState initialState = ConnectionState.Closed;
+            object value;
+
+            try
+            {
+                this.QuietOpen(out initialState);
+                value = this.Command.ExecuteScalar();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                this.QuietClose(initialState);
+            }
+
+            return value;
+        }
+
+        public virtual DataSet ExecuteSql(String Sql)
+        {
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+            this.Command = new SqlCommand(Sql, this.Connection);
+            SqlDataAdapter da = null;
+            DataSet ds;
+            try
+            {
+                da = new SqlDataAdapter(this.Command);
+                ds = new DataSet();
+                da.Fill(ds);
+            }
+            catch (System.Data.SqlClient.SqlException sqlException)
+            {
+                throw sqlException;
+            }
+            catch (System.Exception) // Need ot get specific here 
+            {
+                throw;
+            }
+            finally
+            {
+                if ((da != null))
+                {
+                    da.Dispose();
+                }
+            }
+            return ds;
+        }
+        #endregion
+
+        #region [ Transaction Methods ]
+        public virtual void BeginTransaction()
+        {
+            if ((CurrentTransaction != null))
+            {
+                throw new ApplicationException("There is already a pending transaction. Can not start another one.");
+            }
+            if ((Connection == null))
+            {
+                throw new ApplicationException("Call CreateConnection method before using the connection.");
+            }
+            ConnectionState initialState;
+            try
+            {
+                this.QuietOpen(out initialState);
+                CurrentTransaction = Connection.BeginTransaction();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public virtual void RollbackTransaction()
+        {
+            if ((CurrentTransaction != null))
+            {
+                CurrentTransaction.Rollback();
+                this.QuietClose(ConnectionState.Closed);
+                CurrentTransaction.Dispose();
+            }
+            else
+            {
+                throw new ApplicationException("No transaction to Rollback.");
+            }
+        }
+
+        public virtual void CommitTransaction()
+        {
+            if ((CurrentTransaction != null))
+            {
+                CurrentTransaction.Commit();
+                this.QuietClose(ConnectionState.Closed);
+                CurrentTransaction.Dispose();
+            }
+            else
+            {
+                throw new ApplicationException("No transaction to Commit.");
+            }
+        }
+        #endregion
+
+        #region [ Error Handle ]
+        private void HandleError()
+        {
+        }
+        #endregion
+
+        public virtual void Dispose()
+        {
+            if ((this.Command != null))
+            {
+                this.Command.Dispose();
+            }
+            if ((CurrentTransaction != null))
+            {
+                CurrentTransaction.Rollback();
+                CurrentTransaction.Dispose();
+            }
+            if ((Connection != null))
+            {
+                Connection.Dispose();
+            }
+        }
+
+        #region [ Local Methods ]
+        private void QuietOpen(out ConnectionState initialState)
+        {
+            initialState = ConnectionState.Open;
+            if ((this.Connection.State == ConnectionState.Closed))
+            {
+                initialState = ConnectionState.Closed;
+                this.Connection.Open();
+            }
+        }
+
+        private void QuietClose(ConnectionState initialState)
+        {
+            if ((initialState == ConnectionState.Closed))
+            {
+                this.Connection.Close();
+            }
+        }
+
+        private object GetValue(object value)
+        {
+            if ((value == null))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(byte))
+                        && (((byte)(value)) == byte.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(char))
+                        && (((char)(value)) == char.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(decimal))
+                        && (((decimal)(value)) == decimal.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(double))
+                        && (((double)(value)) == double.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(float))
+                        && (((float)(value)) == float.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(int))
+                        && (((int)(value)) == int.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(long))
+                        && (((long)(value)) == long.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(short))
+                        && (((short)(value)) == short.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(string))
+                        && (((string)(value)) == string.Empty)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(System.DateTime))
+                        && (((System.DateTime)(value)) == System.DateTime.MinValue)))
+            {
+                return DBNull.Value;
+            }
+            if (((value.GetType() == typeof(System.Guid))
+                        && (((System.Guid)(value)) == System.Guid.Empty)))
+            {
+                return DBNull.Value;
+            }
+            return value;
+        }
+        #endregion
+    }
+
+}
--- redisLoad/Program.cs
+++ redisLoad/Program.cs
@@ -11,6 +11,8 @@ using ServiceStack.Redis;
 using ServiceStack.Redis.Generic;
 using System.Data;
 using System.Data.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace redisLoad
 {
@@ -18,26 +20,52 @@ namespace redisLoad
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting Sync .... ");
-
+            Console.WriteLine("Starting Program.....");
+            
+            Console.WriteLine("Parsing Queries File....");
+            
             var program = SqlProgramParser.Parse("queries.txt");
+            
+            Console.WriteLine("Program Parsed...Executing...");
 
+            foreach (var query in program.Queries)
+            {
+                if (GetDataFromSql(program.ConnectionString, query) != 1)
+                {
+                    Console.WriteLine("Error...{0}", query);
+                }
+            }
 
-
-
-            Console.WriteLine("");
+            Console.WriteLine("Program Complete");
         }
 
-
-
-        static int GetDataFromSql(String ConnectionStringName, String Query)
+        static int GetDataFromSql(String ConnectionString, String Query)
         {
             int error = 0;
 
             try
             {
                 Int32 redis_port = 6379;
+                
 
+                // Connect to SQL and Execute the Query 
+                var results = (new DataAccess(ConnectionString)).ExecuteSql(Query);
+                if(results.Tables.Count > 0 && results.Tables[0].Rows.Count > 0)
+                {
+                    Console.WriteLine("Retreived DataSet ( Tables: {0}, Rows: {1} )",results.Tables.Count, results.Tables[0].Rows.Count);
+                    using (var redis = new RedisClient("127.0.0.1",6379))
+                    {
+                        string query_key = CreateCleanQueryKey(Query);
+                        var client = redis.As<String>();
+                        client.SetEntry(query_key, JsonConvert.SerializeObject(results));
+                        Console.WriteLine("{0} saved to redis", query_key);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Retreived DataSet (NO DATA))");
+                }
+                
                 error = 1;
             }
             catch(Exception x)
@@ -49,5 +77,19 @@ namespace redisLoad
             return error;
         }
 
+        static string CreateCleanQueryKey(String query)
+        {
+            string clean = "";
+
+            foreach (var c in query.ToLower())
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    clean = clean + c;
+                }
+            }
+
+            return clean;
+        }
     }
 }
